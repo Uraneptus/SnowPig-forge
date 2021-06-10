637c3c1641c2b955b4d49e1b67702311391bbcd7
Uraneptus <50745606+Uraneptus333@users.noreply.github.com>
2021-06-10 17:46:11 +0200

    working loot tables + frozen ham applies attack damage

---
+++ src/main/java/com/uraneptus/snowpig/common/items/FrozenHam.java
@@ -0,0 +1,27 @@
+package com.uraneptus.snowpig.common.items;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.Item;
+
+public class FrozenHam extends Item {
+    private final Multimap<Attribute, AttributeModifier> defaultModifiers;
+
+    public FrozenHam(int attackDamage, float attackSpeed, Properties properties) {
+        super(properties);
+        ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+        builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Weapon modifier", attackDamage, AttributeModifier.Operation.ADDITION));
+        builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Weapon modifier", attackSpeed, AttributeModifier.Operation.ADDITION));
+        this.defaultModifiers = builder.build();
+    }
+
+    public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlotType slotType) {
+        return slotType == EquipmentSlotType.MAINHAND ? this.defaultModifiers : super.getDefaultAttributeModifiers(slotType);
+    }
+
+
+}
