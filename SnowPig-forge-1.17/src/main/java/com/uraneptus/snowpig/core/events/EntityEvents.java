8b0c41c68a4995830e4a0283c71419c20aedfb4a
Uraneptus <50745606+Uraneptus@users.noreply.github.com>
2021-08-06 14:56:01 +0200

    Delete SnowPig-forge-1.17 directory

--- SnowPig-forge-1.17/src/main/java/com/uraneptus/snowpig/core/events/EntityEvents.java
+++
@@ -1,9 +0,0 @@
-package com.uraneptus.snowpig.core.events;
-
-import com.uraneptus.snowpig.SnowPig;
-import net.minecraftforge.fml.common.Mod;
-
-@Mod.EventBusSubscriber(modid = SnowPig.MOD_ID)
-public class EntityEvents {
-    
-}
