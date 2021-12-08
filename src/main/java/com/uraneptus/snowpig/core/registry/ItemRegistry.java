5607af1ffa7dbfc63d4d6175c14f1aa7c1ef0ac8
Uraneptus <50745606+Uraneptus333@users.noreply.github.com>
2021-12-08 15:49:47 +0100

    Music Disc Update + some version updates

--- src/main/java/com/uraneptus/snowpig/core/registry/ItemRegistry.java
+++ src/main/java/com/uraneptus/snowpig/core/registry/ItemRegistry.java
@@ -18,14 +18,19 @@ public class ItemRegistry {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SnowPig.MOD_ID);
 
-    public static final RegistryObject<Item> FROZEN_PORKCHOP = ITEMS.register("frozen_porkchop", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)
-            .food(new Food.Builder().nutrition(5).saturationMod(0.3f).meat().effect(() -> new EffectInstance(Effects.MOVEMENT_SLOWDOWN, 100, 2), 1.0f).build())));
+    public static final RegistryObject<Item> FROZEN_PORKCHOP = ITEMS.register("frozen_porkchop",
+            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)
+                .food(new Food.Builder().nutrition(5).saturationMod(0.3f).meat()
+                    .effect(() -> new EffectInstance(Effects.MOVEMENT_SLOWDOWN, 100, 2), 1.0f).build())));
 
-    public static final RegistryObject<SnowPigEgg> SNOW_PIG_EGG = ITEMS.register("snow_pig_spawn_egg", () -> new SnowPigEgg(EntityTypeRegistry.SNOW_PIG, 0x70CFF3, 0xE2FBFA,
-            new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<SnowPigEgg> SNOW_PIG_EGG = ITEMS.register("snow_pig_spawn_egg",
+            () -> new SnowPigEgg(EntityTypeRegistry.SNOW_PIG, 0x70CFF3, 0xE2FBFA, new Item.Properties().tab(ItemGroup.TAB_MISC)));
 
     public static final RegistryObject<Item> FROZEN_HAM = ITEMS.register("frozen_ham", () -> new FrozenHam(2,-1.2F, (new Item.Properties()
             .tab(ModList.get().isLoaded("farmersdelight") ? ModIntegrations.getFdItemGroup() : ItemGroup.TAB_FOOD)
             .food(new Food.Builder().nutrition(5).saturationMod(0.3F).effect(() -> new EffectInstance(Effects.MOVEMENT_SLOWDOWN, 100, 2), 1.0F).build()))));
+
+    public static final RegistryObject<Item> MUSIC_DISC_FROSTY_SNIG = ITEMS.register("music_disc_frosty_snig",
+            () -> new MusicDiscItem(12, SoundRegistry.MUSIC_DISC_FROSTY_SNIG, new Item.Properties().stacksTo(1).tab(ItemGroup.TAB_MISC).rarity(Rarity.RARE)));
 
 }
