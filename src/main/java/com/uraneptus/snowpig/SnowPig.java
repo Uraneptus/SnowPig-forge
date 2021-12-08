b004607b8caf85ebbbdc0c541557421398d18fa7
Uraneptus <50745606+Uraneptus333@users.noreply.github.com>
2021-12-08 16:25:50 +0100

    updated forge version

--- src/main/java/com/uraneptus/snowpig/SnowPig.java
+++ src/main/java/com/uraneptus/snowpig/SnowPig.java
@@ -14,8 +14,6 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(SnowPig.MOD_ID)
 @Mod.EventBusSubscriber(modid = SnowPig.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
