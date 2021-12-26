cacb10408f6da9c2a0571735e5560e26bfccc35b
Uraneptus <uraneptus33@gmail.com>
2021-12-26 22:51:19 +0100

    Fd reintegration

--- src/main/java/com/uraneptus/snowpig/core/ModIntegrations.java
+++ src/main/java/com/uraneptus/snowpig/core/ModIntegrations.java
@@ -1,10 +1,11 @@
 package com.uraneptus.snowpig.core;
 
 import net.minecraft.world.item.CreativeModeTab;
+import vectorwing.farmersdelight.FarmersDelight;
 
 public class ModIntegrations {
 
-    /*public static CreativeModeTab getFdItemGroup() {
-        return FarmersDelight.ITEM_GROUP;
-    }*/
+    public static CreativeModeTab getFdItemGroup() {
+        return FarmersDelight.CREATIVE_TAB;
+    }
 }
--- src/main/java/com/uraneptus/snowpig/core/registry/ItemRegistry.java
+++ src/main/java/com/uraneptus/snowpig/core/registry/ItemRegistry.java
@@ -1,10 +1,13 @@
 package com.uraneptus.snowpig.core.registry;
 
 import com.uraneptus.snowpig.SnowPig;
+import com.uraneptus.snowpig.common.items.FrozenHam;
+import com.uraneptus.snowpig.core.ModIntegrations;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.*;
 import net.minecraftforge.common.ForgeSpawnEggItem;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -28,8 +31,8 @@ public class ItemRegistry {
     public static final RegistryObject<Item> MUSIC_DISC_FROSTY_SNIG = ITEMS.register("music_disc_frosty_snig",
             () -> new RecordItem(12, SoundRegistry.MUSIC_DISC_FROSTY_SNIG, new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE)));
 
-    /*public static final RegistryObject<Item> FROZEN_HAM = ITEMS.register("frozen_ham", () -> new FrozenHam(2,-1.2F, (new Item.Properties()
+    public static final RegistryObject<Item> FROZEN_HAM = ITEMS.register("frozen_ham", () -> new FrozenHam(2,-1.2F, (new Item.Properties()
             .tab(ModList.get().isLoaded("farmersdelight") ? ModIntegrations.getFdItemGroup() : CreativeModeTab.TAB_FOOD)
-            .food(new FoodProperties.Builder().nutrition(5).saturationMod(0.3F).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 100, 2), 1.0F).build()))));*/
+            .food(new FoodProperties.Builder().nutrition(5).saturationMod(0.3F).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 100, 2), 1.0F).build()))));
 
 }
