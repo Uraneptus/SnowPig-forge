ae94b715348e4184fe65452c502c8f98ed944b2b
Uraneptus <uraneptus33@gmail.com>
2022-04-15 18:49:14 +0200

    Re added FD compat

--- src/main/java/com/uraneptus/snowpig/core/ModIntegrations.java
+++ src/main/java/com/uraneptus/snowpig/core/ModIntegrations.java
@@ -1,11 +1,11 @@
 package com.uraneptus.snowpig.core;
 
 import net.minecraft.world.item.CreativeModeTab;
-//import vectorwing.farmersdelight.FarmersDelight;
+import vectorwing.farmersdelight.FarmersDelight;
 
 public class ModIntegrations {
 
-    /*public static CreativeModeTab getFdItemGroup() {
+    public static CreativeModeTab getFdItemGroup() {
         return FarmersDelight.CREATIVE_TAB;
-    }*/
+    }
 }
