37cb994962f7ca835bace19b9f8d2a3b85712825
Uraneptus <uraneptus33@gmail.com>
2023-08-01 21:37:31 +0200

    fixed music disc not working

--- src/main/java/com/uraneptus/snowpig/core/data/server/tags/SPItemTagsProvider.java
+++ src/main/java/com/uraneptus/snowpig/core/data/server/tags/SPItemTagsProvider.java
@@ -1,11 +1,13 @@
 package com.uraneptus.snowpig.core.data.server.tags;
 
 import com.uraneptus.snowpig.SnowPigMod;
+import com.uraneptus.snowpig.core.registry.SPItems;
 import com.uraneptus.snowpig.core.tags.SPItemTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -22,5 +24,6 @@ public class SPItemTagsProvider extends ItemTagsProvider {
     protected void addTags(HolderLookup.Provider pProvider) {
         //Mod Tags
         tag(SPItemTags.KNIVES);
+        tag(ItemTags.MUSIC_DISCS).add(SPItems.MUSIC_DISC_FROSTY_SNIG.get());
     }
 }
